07570cbc9d88f24fb29bd50d1d1d543b82eeaec8
Luca <lucabrugnera01@gmail.com>
2024-02-01 14:43:45 +0100

    implements the basic procedures for type inference algorithm

--- src/App/Ast.fs
+++ src/App/Ast.fs
@@ -42,15 +42,17 @@ type scheme = Forall of tyvar Set * ty
 // literals
 //
 
-type lit = LInt of int
-         | LFloat of float
-         | LString of string
-         | LChar of char
-         | LBool of bool
-         | LUnit 
+type lit =
+    | LInt of int
+    | LFloat of float
+    | LString of string
+    | LChar of char
+    | LBool of bool
+    | LUnit
 
-let private (|TyLit|_|) name = function
-    | TyName s when s = name -> Some ()
+let private (|TyLit|_|) name =
+    function
+    | TyName s when s = name -> Some()
     | _ -> None
 
 // active patterns for literal types
@@ -64,9 +66,9 @@ let (|TyUnit|_|) = (|TyLit|_|) "unit"
 // expressions
 //
 
-type binding = bool * string * ty option * expr    // (is_recursive, id, optional_type_annotation, expression)
+type binding = bool * string * ty option * expr // (is_recursive, id, optional_type_annotation, expression)
 
-and expr = 
+and expr =
     | Lit of lit
     | Lambda of string * ty option * expr
     | App of expr * expr
@@ -78,24 +80,26 @@ and expr =
     | UnOp of string * expr
 
 // pseudo constructors for let bindings
-let Let (x, tyo, e1, e2) = LetIn ((false, x, tyo, e1), e2)
-let LetRec (x, tyo, e1, e2) = LetIn ((true, x, tyo, e1), e2)
-   
+let Let (x, tyo, e1, e2) = LetIn((false, x, tyo, e1), e2)
+let LetRec (x, tyo, e1, e2) = LetIn((true, x, tyo, e1), e2)
+
 // active patterns for let bindings
-let (|Let|_|) = function 
-    | LetIn ((false, x, tyo, e1), e2) -> Some (x, tyo, e1, e2)
+let (|Let|_|) =
+    function
+    | LetIn((false, x, tyo, e1), e2) -> Some(x, tyo, e1, e2)
     | _ -> None
-    
-let (|LetRec|_|) = function 
-    | LetIn ((true, x, tyo, e1), e2) -> Some (x, tyo, e1, e2)
+
+let (|LetRec|_|) =
+    function
+    | LetIn((true, x, tyo, e1), e2) -> Some(x, tyo, e1, e2)
     | _ -> None
 
 // environment
 //
 
-type 'a env = (string * 'a) list  
+type 'a env = (string * 'a) list
 
-let lookup env (x : string) = 
+let lookup env (x: string) =
     let _, v = List.find (fun (x', v) -> x = x') env
     v
 
@@ -111,20 +115,23 @@ type value =
 // others
 //
 
-type interactive = IExpr of expr | IBinding of binding
+type interactive =
+    | IExpr of expr
+    | IBinding of binding
 
 // pretty printers
 //
 
-// utility function for printing lists by flattening strings with a separator 
+// utility function for printing lists by flattening strings with a separator
 let rec flatten p sep es =
     match es with
     | [] -> ""
-    | [e] -> p e
+    | [ e ] -> p e
     | e :: es -> sprintf "%s%s %s" (p e) sep (flatten p sep es)
 
 // print pairs within the given env using p as printer for the elements bound within
-let pretty_env p env = sprintf "[%s]" (flatten (fun (x, o) -> sprintf "%s=%s" x (p o)) ";" env)
+let pretty_env p env =
+    sprintf "[%s]" (flatten (fun (x, o) -> sprintf "%s=%s" x (p o)) ";" env)
 
 // print any tuple given a printer p for its elements
 let pretty_tupled p l = flatten p ", " l
@@ -132,7 +139,7 @@ let pretty_tupled p l = flatten p ", " l
 let rec pretty_ty t =
     match t with
     | TyName s -> s
-    | TyArrow (t1, t2) -> sprintf "%s -> %s" (pretty_ty t1) (pretty_ty t2)
+    | TyArrow(t1, t2) -> sprintf "%s -> %s" (pretty_ty t1) (pretty_ty t2)
     | TyVar n -> sprintf "'%d" n
     | TyTuple ts -> sprintf "(%s)" (pretty_tupled pretty_ty ts)
 
@@ -150,39 +157,36 @@ let rec pretty_expr e =
     match e with
     | Lit lit -> pretty_lit lit
 
-    | Lambda (x, None, e) -> sprintf "fun %s -> %s" x (pretty_expr e)
-    | Lambda (x, Some t, e) -> sprintf "fun (%s : %s) -> %s" x (pretty_ty t) (pretty_expr e)
-    
+    | Lambda(x, None, e) -> sprintf "fun %s -> %s" x (pretty_expr e)
+    | Lambda(x, Some t, e) -> sprintf "fun (%s : %s) -> %s" x (pretty_ty t) (pretty_expr e)
+
     // TODO write a better pretty-printer that puts brackets on non-trivial expressions appearing on the right side of an application
-    | App (e1, e2) -> sprintf "%s %s" (pretty_expr e1) (pretty_expr e2)
+    | App(e1, e2) -> sprintf "%s %s" (pretty_expr e1) (pretty_expr e2)
 
     | Var x -> x
 
-    | Let (x, None, e1, e2) ->
-        sprintf "let %s = %s in %s" x (pretty_expr e1) (pretty_expr e2)
+    | Let(x, None, e1, e2) -> sprintf "let %s = %s in %s" x (pretty_expr e1) (pretty_expr e2)
 
-    | Let (x, Some t, e1, e2) ->
-        sprintf "let %s : %s = %s in %s" x (pretty_ty t) (pretty_expr e1) (pretty_expr e2)
+    | Let(x, Some t, e1, e2) -> sprintf "let %s : %s = %s in %s" x (pretty_ty t) (pretty_expr e1) (pretty_expr e2)
 
-    | LetRec (x, None, e1, e2) ->
-        sprintf "let rec %s = %s in %s" x (pretty_expr e1) (pretty_expr e2)
+    | LetRec(x, None, e1, e2) -> sprintf "let rec %s = %s in %s" x (pretty_expr e1) (pretty_expr e2)
 
-    | LetRec (x, Some tx, e1, e2) ->
+    | LetRec(x, Some tx, e1, e2) ->
         sprintf "let rec %s : %s = %s in %s" x (pretty_ty tx) (pretty_expr e1) (pretty_expr e2)
 
-    | IfThenElse (e1, e2, e3o) ->
+    | IfThenElse(e1, e2, e3o) ->
         let s = sprintf "if %s then %s" (pretty_expr e1) (pretty_expr e2)
+
         match e3o with
         | None -> s
         | Some e3 -> sprintf "%s else %s" s (pretty_expr e3)
-        
-    | Tuple es ->        
-        sprintf "(%s)" (pretty_tupled pretty_expr es)
 
-    | BinOp (e1, op, e2) -> sprintf "%s %s %s" (pretty_expr e1) op (pretty_expr e2)
-    
-    | UnOp (op, e) -> sprintf "%s %s" op (pretty_expr e)
-    
+    | Tuple es -> sprintf "(%s)" (pretty_tupled pretty_expr es)
+
+    | BinOp(e1, op, e2) -> sprintf "%s %s %s" (pretty_expr e1) op (pretty_expr e2)
+
+    | UnOp(op, e) -> sprintf "%s %s" op (pretty_expr e)
+
     | _ -> unexpected_error "pretty_expr: %s" (pretty_expr e)
 
 let rec pretty_value v =
@@ -191,17 +195,21 @@ let rec pretty_value v =
 
     | VTuple vs -> pretty_tupled pretty_value vs
 
-    | Closure (env, x, e) -> sprintf "<|%s;%s;%s|>" (pretty_env pretty_value env) x (pretty_expr e)
-    
-    | RecClosure (env, f, x, e) -> sprintf "<|%s;%s;%s;%s|>" (pretty_env pretty_value env) f x (pretty_expr e)
-    
+    | Closure(env, x, e) -> sprintf "<|%s;%s;%s|>" (pretty_env pretty_value env) x (pretty_expr e)
+
+    | RecClosure(env, f, x, e) -> sprintf "<|%s;%s;%s;%s|>" (pretty_env pretty_value env) f x (pretty_expr e)
+
 
 #nowarn "60"
+
 type ty with
-    override self.ToString () = pretty_ty self
+
+    override self.ToString() = pretty_ty self
 
 type expr with
-    override self.ToString () = pretty_expr self
+
+    override self.ToString() = pretty_expr self
 
 type value with
-    override self.ToString () = pretty_value self
+
+    override self.ToString() = pretty_value self
